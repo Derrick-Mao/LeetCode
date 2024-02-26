8911cdecaa2c40b743ea8a87f8bd67f8218b2875
Derrick Mao <derrickmao03@gmail.com>
2024-02-25 17:48:54 -0800

    83 sol

---
+++ Solutions/0083_Rmv_Dupes_from_Sorted_List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode *curr = head;
+        while (curr->next) {
+            if (curr->next->val == curr->val) {
+                ListNode *temp = curr->next;
+                curr->next = temp->next;
+                delete temp;
+            }
+            
+            else {
+                curr = curr->next;
+            }
+        }
+
+        return head;
+    }
+};
